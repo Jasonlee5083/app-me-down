2bba9916d87de429b18635fb0dca56aeb2e90d0e
Simone Nathaniel <shedamoe@gmail.com>
2017-05-09 17:46:26 -0600

    fixed post issue and filter by category

--- public/components/items/items.js
+++ public/components/items/items.js
@@ -1,30 +1,31 @@
 var app = angular.module("appMeDown");
 
-app.controller("itemController", ["$scope", "$http", "itemService", function ($scope, $http, itemService) {
+app.controller("itemController", ["$scope", "$log", "$http", "$uibModal", "itemService", function ($scope, $log, $http, $uibModal, itemService) {
 
 	$scope.items = [];
 
 	itemService.getItems().then(function (response) {
-		return $scope.items = response;
-	})
+        // console.log(response);
+	    return $scope.items = response;
+	});
 
 
 	$scope.submit = function (newItem) {
 		itemService.postItems(newItem).then(function () {
 			$scope.items.push(newItem);
 			console.log($scope.items);
-		})
-	}
+		});
+	};
 
 	$scope.remove = function (index, id) {
 		itemService.removeItems(id).then(function (response) {
 			$scope.items.splice(index, 1);
-		})
-	}
+		});
+	};
 
 	$scope.save = function (newItem) {
 		itemService.saveItems(newItem);
-	}
+	};
 
     $scope.showForm = function () {
         $scope.message = "Show Form Button Clicked";
@@ -32,20 +33,24 @@ app.controller("itemController", ["$scope", "$http", "itemService", function ($s
         var modalInstance = $uibModal.open({
             templateUrl: "add-item-modal.html",
             controller: "itemModalInstanceController",
-            scope:$scope,
+            scope: $scope,
             resolve: {
                 itemForm: function () {
-
                     return $scope.itemForm;
                 }
             }
         });
 
         modalInstance.result.then(function (newItem) {
-            $scope.selected = newItem;
-        }, function () {
-            $log.info("Modal dismissed at: " + new Date());
+            console.log(newItem);
+            itemService.postItems(newItem).then(function(data) {
+                $scope.items.push(data);
+            });
+            // $scope.selected = newItem;
+        }, function (reason) {
+            $log.info(reason);
         });
     };
+
 
 }]);
