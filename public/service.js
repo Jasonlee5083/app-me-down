6d5a0ff2159a0801156ddf108732345bdeeabe28
Simone Nathaniel <shedamoe@gmail.com>
2017-05-09 14:35:43 -0600

    edit post complete

--- public/service.js
+++ public/service.js
@@ -17,13 +17,13 @@ app.service("itemService", ["$http", function ($http) {
     }
 
     this.removeItems = function(id) {
-        return $http.delete("/api/items" + id).then(function(response){
+        return $http.delete("/api/items/" + id).then(function(response){
             return "you item has been deleted"
         })
     }
 
-    this.saveItems = function (todo) {
-        return $http.post("/api/items", newitem).then(function (response) {
+    this.editItems = function (item) {
+        return $http.put("/api/items/" + item._id, item).then(function (response) {
             return response.data;
         }, function (response) {
             alert("Error " + response.status + ": " + response.statusText);
